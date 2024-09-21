44c07e4a1899b57a4e172a9ba2c66b2d0dee17f0
SotaChambers <sota.streamlit@gmail.com>
2024-09-21 18:54:28 +0900

    install amplify

---
+++ frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "filemate0e97823c": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
